2650172a898bbd23d77aae42f0692ea223a99717
serhijko <sp_by@tut.by>
2019-03-05 17:07:09 +0300

    providing Firebase in React with Context

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -17,16 +17,21 @@ import * as ROUTES from '../../constants/routes';
 
 const App = () => (
   <Router>
-    <Navigation />
+    <div>
+      <Navigation />
 
-    <hr />
+      <hr />
 
-    <Route exact path={ROUTES.LANDING} component={LandingPage} />
-    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-    <Route path={ROUTES.HOME} component={HomePage} />
-    <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route
+        path={ROUTES.PASSWORD_FORGET}
+        component={PasswordForgetPage}
+      />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+    </div>
   </Router>
 );
 

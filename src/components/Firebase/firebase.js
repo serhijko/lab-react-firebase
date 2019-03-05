bab0153607a8d3f6710e22c59e58b423ea4c0f8c
serhijko <sp_by@tut.by>
2019-03-05 17:22:58 +0300

    Firebase environment fix

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,12 +1,12 @@
-import { app } from 'firebase';
+import app from 'firebase/app';
 
 const config = {
-  apiKey: process.env.local.REACT_APP_API_KEY,
-  authDomain: process.env.local.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.local.REACT_APP_DATABASE_URL,
-  projectId: process.env.local.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.local.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.local.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase {

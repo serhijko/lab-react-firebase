674493e1a5814a682bcf28b64d03c0e44df6e8ea
serhijko <sp_by@tut.by>
2019-03-06 23:06:59 +0300

    General Authorization and Route Protection

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import { withAuthorization, AuthUserContext } from '../Session';
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
 
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
-export default AccountPage;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);
